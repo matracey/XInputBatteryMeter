0a139dafeb3e93e676040e6447096e08cfa7e37d
Martin Tracey <matracey@outlook.ie>
2017-04-14 01:53:11 +0100

    Completed proof of concept.

---
+++ XInputBatteryMeter/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using SharpDX.XInput;
+
+namespace XInputBatteryMeter
+{
+    public class Program
+    {
+        public Program()
+        {
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine("Initializing.");
+
+            var poller = new XInputWrapperBatteryStatusPoller();
+
+            while (true)
+            {
+
+            }
+
+        }
+    }
+}
